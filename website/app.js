305a560a81950cc42d5ac34d283a95638fff08d4
fujiki <abcdef.nakamura@gmail.com>
2021-01-12 22:15:24 +0900

    [WIP]

--- website/app.js
+++ website/app.js
@@ -3,3 +3,9 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const button = document.getElementById('generate')
+function send () {
+    console.log('send')
+}
+button.addEventListener('click', (event) => { send() })
